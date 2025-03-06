49ec3a09e9d38527d8b4003f24d790a3060b7e6b
Frederik Proske <fproske@gams.com>
2025-03-06 17:01:25 +0100

    Move pod patches code to containerproxy
    This allows pod patches to support SpEL expressions

--- src/main/java/eu/openanalytics/shinyproxy/ShinyProxySpecProvider.java
+++ src/main/java/eu/openanalytics/shinyproxy/ShinyProxySpecProvider.java
@@ -187,12 +187,6 @@ public class ShinyProxySpecProvider implements IProxySpecProvider {
     @Value("${proxy.database.password:${GMS_MIRO_DATABASE_PWD:}}")
     private String dbPass;
 
-    @Value("${proxy.kubernetes.global-pod-patches:#{null}}")
-    private String globalPodPatchesStr;
-
-    @Value("${proxy.kubernetes.global-pod-patches-admin:#{null}}")
-    private String globalPodPatchesAdminStr;
-
     @Inject
     private SpecExpressionResolver expressionResolver;
 
@@ -295,26 +289,6 @@ public class ShinyProxySpecProvider implements IProxySpecProvider {
 
                 specs = specsTmp.stream().map(ShinyProxySpec::getProxySpec).collect(Collectors.toList());
                 specs.forEach(ProxySpec::setContainerIndex);
-                if (containerBackend.equals("kubernetes") && globalPodPatchesStr != null) {
-                    KubernetesSpecExtension extension = KubernetesSpecExtension.builder()
-                            .kubernetesPodPatches(globalPodPatchesStr)
-                            .build();
-                    KubernetesSpecExtension extensionAdminTmp = extension;
-                    if (globalPodPatchesAdminStr != null) {
-                        extensionAdminTmp = KubernetesSpecExtension.builder()
-                                .kubernetesPodPatches(globalPodPatchesAdminStr)
-                                .build();
-                    }
-                    KubernetesSpecExtension extensionAdmin = extensionAdminTmp;
-
-                    specs.forEach(spec -> {
-                        if (spec.getId().equals("admin")) {
-                            spec.addSpecExtension(extensionAdmin);
-                        } else {
-                            spec.addSpecExtension(extension);
-                        }
-                    });
-                }
                 specs.forEach(spec -> specsMap.put(spec.getId(), spec));
                 specs.forEach(spec -> maxInstancesCache.put(spec.getId(), 1));
             }
